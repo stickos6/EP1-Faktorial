1e2d93bad9731f866d0ba9f3abc92b48b0878cef
stickos6 <dejvidmartinik@gmail.com>
2024-05-14 15:35:12 +0200

    readkey (počítá to)

--- EP1-Faktorial/Program.vb
+++ EP1-Faktorial/Program.vb
@@ -3,15 +3,18 @@
 Module Program
     Sub Main(args As String())
 
+        Console.write("Zadej celé číslo pro výpočet faktoriálu: ")
+
         Dim cislo As Integer = Integer.Parse(Console.ReadLine())
         Dim vysledek As Integer = 1
 
-        Console.write("Zadej celé číslo pro výpočet faktoriálu: ")
 
         For x As Integer = 1 To cislo
             vysledek *= x
         Next
 
+        Console.writeline($"Faktoriál čísla {cislo} je {vysledek}")
 
+        Console.ReadKey()
     End Sub
 End Module
